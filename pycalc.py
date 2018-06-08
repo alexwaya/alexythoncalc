042fc7b9b71f1d2479394f1c74d001102e622dec
alexwaya <alex.waya@gmail.com>
2018-06-08 13:01:11 +0300

    add comments

--- pycalc.py
+++ pycalc.py
@@ -1,5 +1,6 @@
 from tkinter import*
 
+
 def iCalc(source, side):
 	storeObj = Frame(source, borderwidth=4, bd=4, bg="powder blue")
 	storeObj.pack(side=side, expand=YES, fill=BOTH)
@@ -10,33 +11,36 @@ def button(source, side, text, command=None):
 	storeObj.pack(side=side, expand=YES, fill=BOTH)
 	return storeObj
 
+#class declaration
 class app(Frame):
-	"""docstring fos app"""
-	def __init__(self, arg):
+	#docstring fos app
+	def __init__(self):
 		#supes app, self).__init__()
 		Frame.__init__(self)
 		self.option_add = ("*Font", "arial 20 bold")
 		self.pack(expand=YES, fill=BOTH)
-		self.master.title("Calculator")
+		self.master.title("Alexython Calculator")
 
-		display = StringVar()
-		ENtry(self, relief=FLAT,
-			textvariable = display, justify ="right", bd=30, bg="powder blue".pack(side=TOP, expand=YES, fill=BOTH))
+		display = StringVar('')
+		dispwc = Entry(self, relief=RIDGE,textvariable = display, justify ="right", bd=30, bg="powder blue")
+		dispwc.pack(side=TOP, expand=YES, fill=BOTH)
 
 
-		for clearBut in xrange(["CE"], ["C"]):
+		for clearBut in (["CE"], ["C"]):
 			erase = iCalc(self,TOP)
-			for ichar in clearBut:
-				button(erase,LEFT,ichar,
-					lambda storeObj=display, q=ichar: storeObj.set(""))
-			
+			for iEquals in clearBut:
+				button(erase,LEFT,iEquals,
+					lambda storeObj=display, q=iEquals: storeObj.set(""))
+		#Adding Text Units	
+
+		
 		for NumBut in ("789/", "456*", "123-", "0.+"):
 			FunctionNum = iCalc(self, TOP)
-			for char in NumBut:
-				button(FunctionNum, LEFT, char,
-					lambda storeObj=display, q=char: storeObj.get() + q)
+			for iEquals in NumBut:
+				button(FunctionNum, LEFT, iEquals,
+					lambda storeObj=display, q=iEquals: storeObj.get() + q)
 
-
+		#Equal Button for calculations
 		EqualsButton = iCalc(self, TOP)
 		for iEquals in "=":
 			if iEquals == "=":
@@ -49,12 +53,14 @@ class app(Frame):
 					lambda storeObj=display, s="%s " %iEquals: storeObj.set(storeObj.get()+s))
 			
 
-
+		#Error Handling
 		def calc(self, display):
 			try:
 				display.set(eval(display.get()))
 			except:
 				display.set("ERROR")
 
+
+#Initialize this App
 if __name__ == '__main__':
 	app().mainloop()
